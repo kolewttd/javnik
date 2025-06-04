7c20a01dd31b7765da9c75878e4048fca2a0141c
kolewttd <kolewtt@proton.me>
2025-06-04 13:27:33 +0200

    A javnik is a public computer such that:
      ssh root@ip
    
    holds with password root.

---
+++ javnik.c
@@ -0,0 +1,24 @@
+/*Copyright (C) 2025 Jakov Perica
+License GPLv3+: GNU GPL version 3 or later <http://gnu.org/licenses/gpl.html>
+This is free software: you are free to change and redistribute it.
+There is NO WARRANTY, to the extent permitted by law.
+*/
+
+#include <stdio.h>
+
+int main() {
+	printf("Copyright (C) 2025 Jakov Perica\n");
+	printf("License GPLv3+: GNU GPL version 3 or later <http://gnu.org/licenses/gpl.html>\n");
+	printf("This is free software: you are free to change and redistribute it.\n");
+	printf("There is NO WARRANTY, to the extent permitted by law.\n\n");
+
+	printf("Definition: Javnik is a public computer such that: \n\n");
+
+	printf("  $ ssh root@ip\n");
+        printf("  Password: root\n\n");
+
+	printf("holds with password root.\n\n");
+
+	printf("Requirements: javnik, free software, TCP/IP, GNU/Linux.\n");
+	printf("Definition: The public internet is a set of javniks.\n\n");
+}
